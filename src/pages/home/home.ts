341ad9ed13d5c8a32acc840ccfde274983ff7573
ntanthavong <62157117+ntanthavong@users.noreply.github.com>
2021-07-25 13:16:00 -0500

    Submitting Homework

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  title = "Grocery List";
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public inputDialogService: InputDialogServiceProvider) {
 
   }
+
+  editItem(item, index){
+    const toast = this.toastCtrl.create({
+      message: "Editing Item - " + item.name,
+      duration: 3000
+    });
+    toast.present();
+    this.inputDialogService.showPrompt(item, index);
+  };
+
+  loadItems(){
+    return this.dataService.getItems();
+  };
+
+  removeItem(item, index){
+    const toast = this.toastCtrl.create({
+      message: "Removing Item - " + item.name,
+      duration: 3000
+    });
+    toast.present();
+    this.dataService.removeItem(index);
+  };
+
+  addItem(){
+    this.inputDialogService.showPrompt();
+  };  
 
 }
